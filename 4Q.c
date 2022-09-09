1e094561811940db46e4d8ad4dfc67a3d0131c3b
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-09 13:55:36 +0530

    Create 4Q.c

---
+++ 4Q.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int prime(int);
+
+int main(){
+
+int n,var;
+
+printf("Enter any number : ");
+
+scanf("%d",&n);
+
+n=n+1;
+
+var=prime(n);
+
+printf("%d",var);
+
+return 0;
+
+}
+
+int prime(int n){
+
+int i=2;
+
+while(i){
+
+for(i=2;i<=n/2;i++){
+
+if(n%i==0){
+
+break;
+
+}
+
+}
+
+if(n%i==0){
+
+n=n+1;
+
+}
+
+else{
+
+return(n);
+
+}
+
+}
+
+}
