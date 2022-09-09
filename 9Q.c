6fba986567bec57ba2f44e1882c83d5c6555d33f
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-09 13:58:37 +0530

    Create 9Q.c

---
+++ 9Q.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int square(int);
+
+int main(){
+
+int n,var;
+
+printf("Enter any number : ");
+
+scanf("%d",&n);
+
+var=square(n);
+
+printf("%d",var);
+
+}
+
+int square(int n){
+
+return(n*n);
+
+}
