374505c12349ef7028f50e7751b80f3b3ae957ad
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-09 13:59:03 +0530

    Create 10Q.c

---
+++ 10Q.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int fact(int);
+
+int main(){
+
+int i,sum=0;
+
+for(i=1;i<=5;i++){
+
+sum=sum+fact(i)/i;
+
+}
+
+printf("%d ",sum);
+
+}
+
+int fact(int i){
+
+int facts=1;
+
+for(i;i>=1;i--){
+
+facts=facts*i;
+
+}
+
+return(facts);
+
+}
