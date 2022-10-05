1e27dfb9f0ffadce734b0788601f397d200df2be
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-06 01:18:22 +0530

    Update 4Q.c

--- 4Q.c
+++ 4Q.c
@@ -1,53 +1,27 @@
 #include<stdio.h>
-
 int prime(int);
-
 int main(){
-
-int n,var;
-
-printf("Enter any number : ");
-
-scanf("%d",&n);
-
-n=n+1;
-
-var=prime(n);
-
-printf("%d",var);
-
-return 0;
-
+    int n,var;
+    printf("Enter any number : ");
+    scanf("%d",&n);
+    n=n+1;
+    var=prime(n);
+    printf("%d",var);
+    return 0;
 }
-
 int prime(int n){
-
-int i=2;
-
-while(i){
-
-for(i=2;i<=n/2;i++){
-
-if(n%i==0){
-
-break;
-
-}
-
-}
-
-if(n%i==0){
-
-n=n+1;
-
-}
-
-else{
-
-return(n);
-
-}
-
-}
-
+    int i=2;
+    while(i){
+        for(i=2;i<=n/2;i++){
+            if(n%i==0){
+                break;
+            }
+        }
+        if(n%i){
+            return(n);
+        }
+        else{
+            n=n+1;   
+        }
+    }
 }
