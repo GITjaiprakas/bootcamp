e81619f9df7e72edf64ab01c6bed3144e48db630
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-06 01:23:20 +0530

    Update 9Q.c

--- 9Q.c
+++ 9Q.c
@@ -1,23 +1,12 @@
 #include<stdio.h>
-
 int square(int);
-
 int main(){
-
 int n,var;
-
 printf("Enter any number : ");
-
 scanf("%d",&n);
-
 var=square(n);
-
 printf("%d",var);
-
 }
-
 int square(int n){
-
 return(n*n);
-
 }
