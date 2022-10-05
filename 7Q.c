f99c4bbcfee8481aeaaf730cbf89d2c7596ee812
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-06 01:20:35 +0530

    Update 7Q.c

--- 7Q.c
+++ 7Q.c
@@ -1,37 +1,19 @@
 #include<stdio.h>
-
-void fib(int );
-
-int main(){
-
-int N;
-
-printf("Enter how many terms you want to print fibonacci series : ");
-
-scanf("%d",&N);
-
-fib(N);
-
-return 0;
-
-}
-
-void fib(int n){
-
-int count=n-2,i=1,k=1;
-
-printf("%d %d ",i,k);
-
-while(count){
-
-k=k+i;
-
-i=k-i;
-
-printf("%d ",k);
-
-count--;
-
-}
-
-}
+    void fib(int );
+    int main(){
+        int N;
+        printf("Enter how many terms you want to print fibonacci series : ");
+        scanf("%d",&N);
+        fib(N);
+        return 0;
+    }
+    void fib(int n){
+        int count=n-2,i=1,k=1;
+        printf("%d %d ",i,k);
+        while(count){
+            k=k+i;
+            i=k-i;
+            printf("%d ",k);
+            count--;
+        }
+    }
