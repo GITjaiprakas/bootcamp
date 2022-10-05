ce321382471d549f1888ba7c2233390e10051fa9
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-06 01:19:00 +0530

    Update 5Q.c

--- 5Q.c
+++ 5Q.c
@@ -1,47 +1,22 @@
 #include<stdio.h>
-
-void prime(int);
-
+int check(int);
 int main(){
-
-int r;
-
-printf("Enter a range of prime numbers : ");
-
-scanf("%d",&r);
-
-prime(r);
-
-return 0;
-
+    int n;
+    printf("Enter the range of numbers : ");
+    scanf("%d",&n);
+    check(n);
+    return 0;
 }
-
-void prime(int r){
-
-int x=2,i;
-
-while(r){
-
-for(i=2;i<x;i++){
-
-if(x%i==0){
-
-break;
-
-}
-
-}
-
-if(i==x){
-
-printf("%d ",x);
-
-r--;
-
-}
-
-x++;
-
-}
-
+int check(int n){
+    int arr[n],i,j;
+    for(i=2;i*i<=n;i++){
+        for(j=2*i;j<=n;j+=i){
+            arr[j]=6;
+        }
+    }
+    for(j=2;j<=n;j++){
+        if(arr[j]!=6){
+            printf("%d ",j);
+        }
+    }
 }
